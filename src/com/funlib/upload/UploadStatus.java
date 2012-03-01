600d32884845a8e82797a0dfb2acad7f71d468ab
tjianli.88@gmail.com <tjianli.88@gmail.com@25efc35d-a909-15da-8fd0-c7407fc77fa2>
2012-03-01 03:20:30 +0000

    修改上传组建

--- src/com/funlib/upload/UploadStatus.java
+++ src/com/funlib/upload/UploadStatus.java
@@ -2,8 +2,7 @@ package com.funlib.upload;
 
 public class UploadStatus {
 
-	public static final int FAIL 				= 	0; /** 上传失败 */
-	public static final int SUCCESS 			= 	1; /** 上传成功 */
+	public static final int FINISH 				= 	0; /** 上传失败 */
 	public static final int CANCELED 			= 	2; /** 取消上传 */
 	public static final int UPLOADING			=	3; /** 正在上传 */
 	public static final int STARTUPLOADING 		= 	4;	/** 开始上传 */
