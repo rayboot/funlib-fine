e95ac3f0b120e0a3ab7f4cf395d751d403a0a087
sy0725work@gmail.com <sy0725work@gmail.com@25efc35d-a909-15da-8fd0-c7407fc77fa2>
2012-03-09 11:01:29 +0000

    add effect

--- trunk/src/com/funlib/imagefilter/ImageUtily.java
+++ trunk/src/com/funlib/imagefilter/ImageUtily.java
@@ -1,6 +1,8 @@
 package com.funlib.imagefilter;
 
 import java.io.ByteArrayOutputStream;
+
+import android.R.integer;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
@@ -22,19 +24,164 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 
 public class ImageUtily {
+
+	public enum EffectType {
+		ChannelBlend_Normal, 
+		ChannelBlend_Lighten, 
+		ChannelBlend_Darken, 
+		ChannelBlend_Multiply,
+		ChannelBlend_Average,
+		ChannelBlend_Add,
+		ChannelBlend_Subtract,
+		ChannelBlend_Difference,
+		ChannelBlend_Negation,
+		ChannelBlend_Screen,
+		ChannelBlend_Exclusion,
+		ChannelBlend_Overlay,
+		ChannelBlend_SoftLight,
+		ChannelBlend_HardLight,
+		ChannelBlend_ColorDodge,
+		ChannelBlend_ColorBurn,
+		ChannelBlend_LinearDodge,
+		ChannelBlend_LinearBurn,
+		ChannelBlend_LinearLight,
+		ChannelBlend_VividLight,
+		ChannelBlend_PinLight,
+		ChannelBlend_HardMix,
+		ChannelBlend_Reflect,
+		ChannelBlend_Glow,
+		ChannelBlend_Phoenix,
+		ChannelBlend_Alpha,
+		ChannelBlend_Devide,
+	}
 	
+	public static int getEffectFun(int src, int mask, EffectType effectFun, float maskAlpha){
+		switch (effectFun) {
+		case ChannelBlend_Normal:
+			return src;
+		case ChannelBlend_Lighten:
+			return ps_lightenFun(src, mask);
+		case ChannelBlend_Darken:
+			return ps_darkenFun(src, mask);
+		case ChannelBlend_Multiply:
+			return ps_multiplyFun(src, mask);
+		case ChannelBlend_Average:
+			return ps_averageFun(src, mask);
+		case ChannelBlend_Add:
+			return ps_addFun(src, mask);
+		case ChannelBlend_Subtract:
+			return ps_subtractFun(src, mask);
+		case ChannelBlend_Difference:
+			return ps_differenceFun(src, mask);
+		case ChannelBlend_Negation:
+			return ps_negationFun(src, mask);
+		case ChannelBlend_Screen:
+			return ps_screenFun(src, mask);
+		case ChannelBlend_Exclusion:
+			return ps_ExclusionFun(src, mask);
+		case ChannelBlend_Overlay:
+			return ps_overlayFun(src, mask);
+		case ChannelBlend_SoftLight:
+			return ps_softlightFun(src, mask);
+		case ChannelBlend_HardLight:
+			return ps_hardlightFun(src, mask);
+		case ChannelBlend_ColorDodge:
+			return ps_ColorDodgeFun(src, mask);
+		case ChannelBlend_ColorBurn:
+			return ps_ColorBurnFun(src, mask);
+		case ChannelBlend_LinearDodge:
+			return ps_LinearDodgeFun(src, mask);
+		case ChannelBlend_LinearBurn:
+			return ps_LinearBurnFun(src, mask);
+		case ChannelBlend_LinearLight:
+			return ps_LinearLightFun(src, mask);
+		case ChannelBlend_VividLight:
+			return ps_VividLightFun(src, mask);
+		case ChannelBlend_PinLight:
+			return ps_PinLightFun(src, mask);
+		case ChannelBlend_HardMix:
+			return ps_HardMixFun(src, mask);
+		case ChannelBlend_Reflect:
+			return ps_ReflectFun(src, mask);
+		case ChannelBlend_Glow:
+			return ps_GlowFun(src, mask);
+		case ChannelBlend_Phoenix:
+			return ps_PhoenixFun(src, mask);
+		case ChannelBlend_Alpha:
+			return ps_AlphaFun(src, mask, maskAlpha);
+		case ChannelBlend_Devide:
+			return ps_DevideFun(src, mask);
+		default:
+			break;
+		}
+		return 0;
+	}
+	
+	public static Bitmap ps_getEffectBitmap(EffectType iType, Bitmap src, Bitmap mask, float maskAlpha) {
+		int width = src.getWidth();
+		int height = src.getHeight();
+		Bitmap bitmap = Bitmap.createBitmap(width, height,
+				Bitmap.Config.ARGB_8888);
+		int pixColorSrc = 0;
+		int pixColorMask = 0;
+		int RSrc = 0;
+		int GSrc = 0;
+		int BSrc = 0;
+		int RMask = 0;
+		int GMask = 0;
+		int BMask = 0;
+		int index = 0;
+		int[] pixelsSrc = new int[width * height];
+		int[] pixelsMask = new int[width * height];
+		src.getPixels(pixelsSrc, 0, width, 0, 0, width, height);
+		mask.getPixels(pixelsMask, 0, width, 0, 0, width, height);
+		for (int i = 0; i < height; i++) {
+			for (int k = 0; k < width; k++) {
+
+				index = width * i + k;
+				pixColorSrc = pixelsSrc[index];
+				pixColorMask = pixelsMask[index];
+				RSrc = Color.red(pixColorSrc);
+				GSrc = Color.green(pixColorSrc);
+				BSrc = Color.blue(pixColorSrc);
+				RMask = Color.red(pixColorMask);
+				GMask = Color.green(pixColorMask);
+				BMask = Color.blue(pixColorMask);
+
+				RSrc = getEffectFun(RSrc, RMask, iType, maskAlpha);
+				if (maskAlpha > Float.MIN_VALUE) {
+					RSrc = getEffectFun(RSrc, RMask, EffectType.ChannelBlend_Alpha, maskAlpha);
+				}
+				GSrc = getEffectFun(GSrc, GMask, iType, maskAlpha);
+				if (maskAlpha > Float.MIN_VALUE) {
+					GSrc = getEffectFun(GSrc, GMask, EffectType.ChannelBlend_Alpha, maskAlpha);
+				}
+				BSrc = getEffectFun(BSrc, BMask, iType, maskAlpha);
+				if (maskAlpha > Float.MIN_VALUE) {
+					BSrc = getEffectFun(BSrc, BMask, EffectType.ChannelBlend_Alpha, maskAlpha);
+				}
+				pixelsSrc[index] = Color.argb(255, RSrc, GSrc, BSrc);
+			}
+		}
+
+		bitmap.setPixels(pixelsSrc, 0, width, 0, 0, width, height);
+		return bitmap;
+	}
+
 	/**
 	 * 模仿ps 柔光效果
+	 * 
 	 * @param src
 	 * @param mask
 	 * @param maskAlpha
 	 * @return
 	 */
-	public static Bitmap ps_softLight(Bitmap src , Bitmap mask , float maskAlpha){
-		
+	public static Bitmap ps_softLight(Bitmap src, Bitmap mask, float maskAlpha) {
+
 		int width = src.getWidth();
 		int height = src.getHeight();
-		Bitmap bitmap = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888);
+		Bitmap bitmap = Bitmap.createBitmap(width, height,
+				Bitmap.Config.ARGB_8888);
 		int pixColorSrc = 0;
 		int pixColorMask = 0;
 		int RSrc = 0;
@@ -50,8 +197,8 @@ public class ImageUtily {
 		mask.getPixels(pixelsMask, 0, width, 0, 0, width, height);
 		for (int i = 0; i < height; i++) {
 			for (int k = 0; k < width; k++) {
-				
-				index = width*i + k; 
+
+				index = width * i + k;
 				pixColorSrc = pixelsSrc[index];
 				pixColorMask = pixelsMask[index];
 				RSrc = Color.red(pixColorSrc);
@@ -60,10 +207,10 @@ public class ImageUtily {
 				RMask = Color.red(pixColorMask);
 				GMask = Color.green(pixColorMask);
 				BMask = Color.blue(pixColorMask);
-				
-				RSrc = ps_softLightFun(RSrc, RMask , maskAlpha);
-				GSrc = ps_softLightFun(GSrc, GMask , maskAlpha);
-				BSrc = ps_softLightFun(BSrc, BMask , maskAlpha);
+
+				RSrc = ps_softLightFun(RSrc, RMask, maskAlpha);
+				GSrc = ps_softLightFun(GSrc, GMask, maskAlpha);
+				BSrc = ps_softLightFun(BSrc, BMask, maskAlpha);
 				pixelsSrc[index] = Color.argb(255, RSrc, GSrc, BSrc);
 			}
 		}
@@ -71,24 +218,29 @@ public class ImageUtily {
 		bitmap.setPixels(pixelsSrc, 0, width, 0, 0, width, height);
 		return bitmap;
 	}
-	private static int ps_softLightFun(int src , int mask , float maskAlpha){
-		
-		int value = (int) (((src < 128)?(2*((mask>>1)+64))*((float)src/255):(255-(2*(255-((mask>>1)+64))*(float)(255-src)/255))));
-		return (int) ((1-maskAlpha)*src + maskAlpha*value);
+
+	private static int ps_softLightFun(int src, int mask, float maskAlpha) {
+
+		int value = (int) (((src < 128) ? (2 * ((mask >> 1) + 64))
+				* ((float) src / 255) : (255 - (2 * (255 - ((mask >> 1) + 64))
+				* (float) (255 - src) / 255))));
+		return (int) ((1 - maskAlpha) * src + maskAlpha * value);
 	}
-	
+
 	/**
 	 * 模仿ps 排除效果
+	 * 
 	 * @param src
 	 * @param mask
 	 * @param maskAlpha
 	 * @return
 	 */
-	public static Bitmap ps_exclusion(Bitmap src , Bitmap mask , float maskAlpha){
-		
+	public static Bitmap ps_exclusion(Bitmap src, Bitmap mask, float maskAlpha) {
+
 		int width = src.getWidth();
 		int height = src.getHeight();
-		Bitmap bitmap = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888);
+		Bitmap bitmap = Bitmap.createBitmap(width, height,
+				Bitmap.Config.ARGB_8888);
 		int pixColorSrc = 0;
 		int pixColorMask = 0;
 		int RSrc = 0;
@@ -104,8 +256,8 @@ public class ImageUtily {
 		mask.getPixels(pixelsMask, 0, width, 0, 0, width, height);
 		for (int i = 0; i < height; i++) {
 			for (int k = 0; k < width; k++) {
-				
-				index = width*i + k; 
+
+				index = width * i + k;
 				pixColorSrc = pixelsSrc[index];
 				pixColorMask = pixelsMask[index];
 				RSrc = Color.red(pixColorSrc);
@@ -114,10 +266,10 @@ public class ImageUtily {
 				RMask = Color.red(pixColorMask);
 				GMask = Color.green(pixColorMask);
 				BMask = Color.blue(pixColorMask);
-				
-				RSrc = ps_exclusionFun(RSrc, RMask , maskAlpha);
-				GSrc = ps_exclusionFun(GSrc, GMask , maskAlpha);
-				BSrc = ps_exclusionFun(BSrc, BMask , maskAlpha);
+
+				RSrc = ps_exclusionFun(RSrc, RMask, maskAlpha);
+				GSrc = ps_exclusionFun(GSrc, GMask, maskAlpha);
+				BSrc = ps_exclusionFun(BSrc, BMask, maskAlpha);
 				pixelsSrc[index] = Color.argb(255, RSrc, GSrc, BSrc);
 			}
 		}
@@ -125,53 +277,172 @@ public class ImageUtily {
 		bitmap.setPixels(pixelsSrc, 0, width, 0, 0, width, height);
 		return bitmap;
 	}
-	private static int ps_exclusionFun(int src , int mask , float maskAlpha){
-		
-		int value = (int) ((src + mask - 2 * src * mask / 255));
-		return (int) ((1-maskAlpha)*src + maskAlpha*value);
+
+	private static int ps_lightenFun(int src, int mask) {
+		return mask > src ? mask : src;
 	}
-	
+
+	private static int ps_darkenFun(int src, int mask) {
+		return mask > src ? src : mask;
+	}
+
+	private static int ps_multiplyFun(int src, int mask) {
+		return mask * src / 255;
+	}
+
+	private static int ps_averageFun(int src, int mask) {
+		return (src + mask) >> 1;
+	}
+
+	private static int ps_addFun(int src, int mask) {
+		return (src + mask > 255) ? 255 : src + mask;
+	}
+
+	private static int ps_subtractFun(int src, int mask) {
+		return (src + mask < 255) ? 0 : (src + mask - 255);
+	}
+
+	private static int ps_differenceFun(int src, int mask) {
+		return (src > mask) ? (src - mask) : (mask - src);
+	}
+
+	private static int ps_negationFun(int src, int mask) {
+		return (src + mask > 255) ? (src + mask) : (255 + 255 - src - mask);
+	}
+
+	private static int ps_screenFun(int src, int mask) {
+		return 255 - (((255 - src) * (255 - mask)) >> 8);
+	}
+
+	private static int ps_ExclusionFun(int src, int mask) {
+		return ((int)(src + mask - 2 * src * mask / 255));
+	}
+
+	private static int ps_overlayFun(int src, int mask) {
+		return ((int) ((mask < 128) ? (2 * src * mask / 255) : (255 - 2
+				* (255 - src) * (255 - mask) / 255)));
+	}
+
+	private static int ps_softlightFun(int src, int mask) {
+		return ((int) ((mask < 128) ? (2 * ((src >> 1) + 64))
+				* ((float) mask / 255) : (255 - (2 * (255 - ((src >> 1) + 64))
+				* (float) (255 - mask) / 255))));
+	}
+
+	private static int ps_hardlightFun(int src, int mask) {
+		return (ps_overlayFun(mask, src));
+	}
+
+	private static int ps_ColorDodgeFun(int src, int mask) {
+		return ((int) ((mask == 255) ? mask : Math.min(255,
+				((src << 8) / (255 - mask)))));
+	}
+
+	private static int ps_ColorBurnFun(int src, int mask) {
+		return ((int) ((mask == 0) ? mask : Math.max(0,
+				(255 - ((255 - src) << 8) / mask))));
+	}
+
+	private static int ps_LinearDodgeFun(int src, int mask) {
+		return ps_addFun(src, mask);
+	}
+
+	private static int ps_LinearBurnFun(int src, int mask) {
+		return ps_subtractFun(src, mask);
+	}
+
+	private static int ps_LinearLightFun(int src, int mask) {
+		return ((int) ((mask < 128) ? ps_LinearBurnFun(src, (2 * mask))
+				: ps_LinearDodgeFun(src, (2 * (mask - 128)))));
+	}
+
+	private static int ps_VividLightFun(int src, int mask) {
+		return ((int) ((mask < 128) ? ps_ColorBurnFun(src, (2 * mask))
+				: ps_ColorDodgeFun(src, (2 * (mask - 128)))));
+	}
+
+	private static int ps_PinLightFun(int src, int mask) {
+		return ((int) ((mask < 128) ? ps_darkenFun(src, (2 * mask))
+				: ps_lightenFun(src, (2 * (mask - 128)))));
+	}
+
+	private static int ps_HardMixFun(int src, int mask) {
+		return ((int) ((ps_VividLightFun(src, mask) < 128) ? 0 : 255));
+	}
+
+	private static int ps_ReflectFun(int src, int mask) {
+		return ((int) ((mask == 255) ? mask : Math.min(255,
+				(src * src / (255 - mask)))));
+	}
+
+	private static int ps_GlowFun(int src, int mask) {
+		return ps_ReflectFun(mask, src);
+	}
+
+	private static int ps_PhoenixFun(int src, int mask) {
+		return ((int) (Math.min(src, mask) - Math.max(src, mask) + 255));
+	}
+
+	private static int ps_AlphaFun(int src, int mask, float maskAlpha) {
+		return ((int) (maskAlpha * src + (1 - maskAlpha) * mask));
+	}
+
+	private static int ps_DevideFun(int src, int mask) {
+		return Math.min(255, (int)(src/((float)(mask)/255)));
+	}
+
+	private static int ps_exclusionFun(int src, int mask, float maskAlpha) {
+
+		int value = (int) ((src + mask - 2 * src * mask / 255));
+		return (int) ((1 - maskAlpha) * src + maskAlpha * value);
+	}
+
 	/**
 	 * 修改图片亮度
+	 * 
 	 * @param bmp
-	 * @param degress 亮度[-255, 255]
+	 * @param degress
+	 *            亮度[-255, 255]
 	 * @return
 	 */
-	public static Bitmap changeBrightness(Bitmap bmp , int brightness){
-		
-		Bitmap bitmap = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(),  Config.ARGB_8888);  
-        ColorMatrix cMatrix = new ColorMatrix();  
-        cMatrix.set(new float[] { 1, 0, 0, 0, brightness, 0, 1,  
-                0, 0, brightness,// 改变亮度  
-                0, 0, 1, 0, brightness, 0, 0, 0, 1, 0 });  
+	public static Bitmap changeBrightness(Bitmap bmp, int brightness) {
 
-        Paint paint = new Paint();  
-        paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));  
-        Canvas canvas = new Canvas(bitmap);  
-        canvas.drawBitmap(bmp, 0, 0, paint);  
-        
-        return bitmap;
+		Bitmap bitmap = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(),
+				Config.ARGB_8888);
+		ColorMatrix cMatrix = new ColorMatrix();
+		cMatrix.set(new float[] { 1, 0, 0, 0, brightness, 0, 1, 0, 0,
+				brightness,// 改变亮度
+				0, 0, 1, 0, brightness, 0, 0, 0, 1, 0 });
+
+		Paint paint = new Paint();
+		paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));
+		Canvas canvas = new Canvas(bitmap);
+		canvas.drawBitmap(bmp, 0, 0, paint);
+
+		return bitmap;
 	}
-	
+
 	/**
 	 * 修改对比度
+	 * 
 	 * @param bmp
-	 * @param contrast [0-1]
+	 * @param contrast
+	 *            [0-1]
 	 * @return
 	 */
-	public static Bitmap changeContrast(Bitmap bmp, float contrast){
-		
-		Bitmap bitmap = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(),  Config.ARGB_8888);
-        ColorMatrix cMatrix = new ColorMatrix();  
-        cMatrix.set(new float[] { contrast, 0, 0, 0, 0, 0,  
-        		contrast, 0, 0, 0,// 改变对比度  
-                0, 0, contrast, 0, 0, 0, 0, 0, 1, 0 });  
+	public static Bitmap changeContrast(Bitmap bmp, float contrast) {
 
-        Paint paint = new Paint();  
-        paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));  
-        Canvas canvas = new Canvas(bitmap);  
-        canvas.drawBitmap(bmp, 0, 0, paint);
-        return bitmap;
+		Bitmap bitmap = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(),
+				Config.ARGB_8888);
+		ColorMatrix cMatrix = new ColorMatrix();
+		cMatrix.set(new float[] { contrast, 0, 0, 0, 0, 0, contrast, 0, 0, 0,// 改变对比度
+				0, 0, contrast, 0, 0, 0, 0, 0, 1, 0 });
+
+		Paint paint = new Paint();
+		paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));
+		Canvas canvas = new Canvas(bitmap);
+		canvas.drawBitmap(bmp, 0, 0, paint);
+		return bitmap;
 	}
 
 	/**
@@ -336,7 +607,6 @@ public class ImageUtily {
 		return null;
 	}
 
-
 	/**
 	 * 给图片添加相框，mask要是png格式，中间部分支持透明 如果src图片尺寸小于mask，会对src按照mask比例缩放
 	 * 
@@ -371,31 +641,32 @@ public class ImageUtily {
 		layer.draw(canvas);
 		return bitmap;
 	}
-	
+
 	/**
 	 * 给图片添加指定宽高的黑色背景框
+	 * 
 	 * @param bitmap
 	 * @param bgW
 	 * @param bgH
 	 * @return
 	 */
-	public static Bitmap addBitmapBlackBG(Bitmap bitmap , int bgW , int bgH){
-		
+	public static Bitmap addBitmapBlackBG(Bitmap bitmap, int bgW, int bgH) {
+
 		try {
-			
+
 			int x = 0;
 			int y = 0;
 			int bmpW = bitmap.getWidth();
 			int bmpH = bitmap.getHeight();
-			x = (bgW - bmpW)/2;
-			y = (bgH - bmpH)/2;
-			
-			Bitmap output = Bitmap.createBitmap(bgW , bgH , Config.RGB_565);
+			x = (bgW - bmpW) / 2;
+			y = (bgH - bmpH) / 2;
+
+			Bitmap output = Bitmap.createBitmap(bgW, bgH, Config.RGB_565);
 			Canvas canvas = new Canvas(output);
 			final Paint paint = new Paint();
 			paint.setAntiAlias(true);
 			canvas.drawARGB(0, 0, 0, 0);
-			canvas.drawBitmap(bitmap , x , y , paint);
+			canvas.drawBitmap(bitmap, x, y, paint);
 			return output;
 		} catch (Exception e) {
 			return bitmap;
@@ -413,7 +684,7 @@ public class ImageUtily {
 		PointF pf = new PointF();
 		BitmapFactory.Options opt = new BitmapFactory.Options();
 		opt.inJustDecodeBounds = true;
-		BitmapFactory.decodeByteArray(data, 0, data.length , opt);
+		BitmapFactory.decodeByteArray(data, 0, data.length, opt);
 		pf.x = opt.outWidth;
 		pf.y = opt.outHeight;
 
